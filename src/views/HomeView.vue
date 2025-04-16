0242ec931e965d7395269668d2ddb323f22bfeab
Sun Sunny <sol16821@gmail.com>
2025-04-16 18:01:13 +0200

    Do basic HomeView.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,52 @@
 <script setup>
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
     <article>
-        <h1>What do you wanna do today?</h1>
-
+        <div class="section-title">
+            <h1>What do you wanna do today?</h1>
+        </div>
+        <div class="section-info">
+            <RouterView />
+        </div>
+    </article>
+    <article>
+        <div class="section-title">
+            <h1>Current tasks</h1>
+        </div>
+        <div class="section-info">
+            <RouterView />
+        </div>
+    </article>
+    <article>
+        <div class="section-title">
+            <h1>Completed tasks</h1>
+        </div>
+        <div class="section-info">
+            <RouterView />
+        </div>
     </article>
 </template>
 
 <style>
     article {
         margin: 1rem;
+        
+        .section-title {
+            background-color: var(--medium-green);
+            border-radius: 12px 12px 0 0;
 
-        h1 {
-            padding: 5px;
-            background-color: var(--light-green);
-            font-size: 1rem;
-            color: white;
+            h1 {
+                margin-left: 5px;
+                padding: 5px;
+                font-size: 1rem;
+                color: white;
+            }
+        }
+
+        .section-info {
+            background-color: white;
         }
     }
 </style>
