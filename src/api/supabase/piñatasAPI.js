a426cd83ac34e77c92543e4140f0f48ccd4857f3
Sun Sunny <sol16821@gmail.com>
2025-04-16 21:05:45 +0200

    Add supabase functions to piñataAPI.js and fix supabase.js

--- src/api/supabase/piñatasAPI.js
+++ src/api/supabase/piñatasAPI.js
@@ -10,10 +10,65 @@ export const getPiñatas = async () => {
         if (error) {
             throw new Error(error.message)
         }
-        
+
         return data
     } catch (err) {
         console.error(err)
         return [];
+    }
+}
+
+//Create user
+export const signUp = async (email, password) => {
+    try {
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password
+        })
+
+        if (error) {
+            throw new Error(error.message)
+        }
+
+        return data
+    } catch (err) {
+        console.error(err)
+
+        return null
+    }
+}
+
+//login user
+export const login = async (email, password) => {
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+
+        if (error) {
+            throw new Error(error.message)
+        }
+
+        return data
+    } catch (err) {
+        console.error(err)
+
+        return null
+    }
+}
+
+//Delete user
+export const deleteUser = async (userId) => {
+    try {
+        const { error } = await supabase.auth.admin.deleteUser(userId)
+
+        if (error) {
+            throw new Error(error.message)
+        }
+
+        
+    } catch (err) {
+        console.error(err)
     }
 }
