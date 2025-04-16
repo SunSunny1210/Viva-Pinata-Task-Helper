876d0c925a8f18a663b38e056ce6d6b62f0b0725
Sun Sunny <sol16821@gmail.com>
2025-04-16 14:49:29 +0200

    Do basic navbar.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,55 @@
 <script setup>
-import VPLogo from '@/assets/VivaPinataTIP.png'
+import VPLogo from '@/assets/VivaPinataTIP.png';
+import Pretzail from '@/assets/pretzail.jpg'
 import { ref } from 'vue';
 
 </script>
 
 <template>
     <header>
-        <img :src=VPLogo />
+        <div class="menu-btn">
+            <span><</span>
+            <button :style="{ backgroundImage: `url(${Pretzail})` }"></button>
+        </div>
+        <img class="logo" :src=VPLogo />
     </header>
 </template>
 
 <style scoped>
+    header {
+        height: 17vh;
+        background-color: rgb(186, 250, 90);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        
+        .logo {
+            margin-right: 1.5rem;
+            height: 80%;
+            z-index: 1;
+        }
+        
+        .menu-btn {
+            height: 60%;
+            width: 30%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgb(90, 235, 0);
+            border-radius: 0 12% 12% 0;
+            font-size: 2rem;
+            color: white;
+
+            button {
+                margin-left: 1rem;
+                height: 80%;
+                width: 60%;
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;
+                border: 3px solid yellow;
+                border-radius: 12%;
+            }
+        }
+    }
 </style>
