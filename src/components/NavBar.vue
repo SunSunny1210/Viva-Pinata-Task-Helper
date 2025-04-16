79118378c888afb1b07ff0c2658c84105dbdac98
Sun Sunny <sol16821@gmail.com>
2025-04-16 13:08:32 +0200

    Add logo and start with navbar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+import VPLogo from '@/assets/VivaPinataTIP.png'
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <header>
+        <img :src=VPLogo />
+    </header>
+</template>
+
+<style scoped>
+</style>
