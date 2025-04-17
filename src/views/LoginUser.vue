f3f9f0eff10995359524b48414a33078953067de
Sun Sunny <sol16821@gmail.com>
2025-04-17 23:19:45 +0200

    Make basic structure to LoginUser and RegisterUser.

---
+++ src/views/LoginUser.vue
@@ -0,0 +1,13 @@
+<script setup>
+import UserForm from '@/components/UserForm.vue';
+
+</script>
+
+<template>
+    <h1>Log in!</h1>
+    <p>Enter your email and password to log in. Remember your password!</p>
+    <UserForm />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,13 @@
+<script setup>
+import UserForm from '@/components/UserForm.vue';
+
+</script>
+
+<template>
+    <h1>Register your user!</h1>
+    <p>Enter your email and password to create your user. Remember to make a safe password!</p>
+    <UserForm />
+</template>
+
+<style scoped>
+</style>
