3dd489d985a3ca810ee940d6116fab48a023e6dd
Sun Sunny <sol16821@gmail.com>
2025-04-17 23:16:42 +0200

    Create basic routes to different views.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterUser from '@/views/RegisterUser.vue'
+import LoginUser from '@/views/LoginUser.vue'
+import CreateProfile from '@/views/CreateProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +11,22 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginUser,
+    },
+    {
+      path: '/create-profile',
+      name: 'create-profile',
+      component: CreateProfile,
+    },
   ],
 })
 
