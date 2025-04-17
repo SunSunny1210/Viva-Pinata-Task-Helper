95c010bc08efeb985537d77fe97e368b1fade684
Sun Sunny <sol16821@gmail.com>
2025-04-18 01:09:18 +0200

    Make basic UserData structure.

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,8 +1,57 @@
 <script setup>
+import { useUserStore } from '@/stores/store';
+import { h } from 'vue';
+import Pretzail from '@/assets/pretzail.jpg'
+
+const storeUser = useUserStore();
+
+const printUserTemplate = () =>{
+    return storeUser.profileData
+    ? h("div", { class: "user-info" }, [
+        h("img", { src: `${storeUser.profileData.avatar_url}` }),
+        h("div", { class: "profile-names" }, [
+            h("h3", { class: "username" }, storeUser.profileData.username),
+            h("h4", { class: "farm-name" }, storeUser.profileData.farm_name)
+        ])
+    ])
+    : h("div", { class: "no-user" }, [
+        h("img", { src: `${Pretzail}` }),
+        h("div", { class: "options" }, [
+            h("RouterLink", { to: "/register" }, "Register"),
+            h("RouterLink", { to: "/login" }, "Log in")
+        ])
+    ])
+    
+}
 </script>
 
 <template>
+    <printUserTemplate />
 </template>
 
-<style scoped>
+<style>
+    .no-user {
+        height: 20%;
+        width: 109%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--medium-green);
+
+        img {
+            height: 100px;
+        }
+
+        .options {
+            margin-left: 10px;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            gap: 15px;
+            align-items: flex-start;
+            font-size: 1.5rem;
+            color: white;
+        }
+    }
 </style>
