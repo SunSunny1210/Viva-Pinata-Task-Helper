ce3c15b837316b3fea537d614948766ea4f76821
Sun Sunny <sol16821@gmail.com>
2025-04-17 23:09:16 +0200

    Make basic UserForm.

---
+++ src/components/UserForm.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="form">
+        <form>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" />
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </form>
+    </div>
+</template>
+
+<style scoped>
+</style>
